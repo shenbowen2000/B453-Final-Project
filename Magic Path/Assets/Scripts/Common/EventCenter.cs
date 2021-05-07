d863952ec9407b655fa1b2a114dcc6e1f53656fc
shenbowen2000 <54719197+shenbowen2000@users.noreply.github.com>
2021-05-06 22:58:12 -0400

    Magic Path

---
+++ Magic Path/Assets/Scripts/Common/EventCenter.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventCenter
+{
+    private static Dictionary<EventDefine, Delegate> m_EventTable = new Dictionary<EventDefine, Delegate>();
+
+    private static void OnListenerAdding(EventDefine eventType, Delegate callBack)
+    {
+        if (!m_EventTable.ContainsKey(eventType))
+        {
+            m_EventTable.Add(eventType, null);
+        }
+        Delegate d = m_EventTable[eventType];
+        if (d != null && d.GetType() != callBack.GetType())
+        {
+            throw new Exception(string.Format("Try to add event {0}，the event now is event {1}，the event is going to add is event {2}", eventType, d.GetType(), callBack.GetType()));
+        }
+    }
+    private static void OnListenerRemoving(EventDefine eventType, Delegate callBack)
+    {
+        if (m_EventTable.ContainsKey(eventType))
+        {
+            Delegate d = m_EventTable[eventType];
+            if (d == null)
+            {
+                throw new Exception(string.Format("Remove listener", eventType));
+            }
+            else if (d.GetType() != callBack.GetType())
+            {
+                throw new Exception(string.Format("Remove listener: Try to remove event {0}, the event now is event{1}, the event is going to remove is event{2}", eventType, d.GetType(), callBack.GetType()));
+            }
+        }
+        else
+        {
+            throw new Exception(string.Format("Remove listener: No event {0}", eventType));
+        }
+    }
+    private static void OnListenerRemoved(EventDefine eventType)
+    {
+        if (m_EventTable[eventType] == null)
+        {
+            m_EventTable.Remove(eventType);
+        }
+    }
+    //no parameters
+    public static void AddListener(EventDefine eventType, CallBack callBack)
+    {
+        OnListenerAdding(eventType, callBack);
+        m_EventTable[eventType] = (CallBack)m_EventTable[eventType] + callBack;
+    }
+    //Single parameters
+    public static void AddListener<T>(EventDefine eventType, CallBack<T> callBack)
+    {
+        OnListenerAdding(eventType, callBack);
+        m_EventTable[eventType] = (CallBack<T>)m_EventTable[eventType] + callBack;
+    }
+    //two parameters
+    public static void AddListener<T, X>(EventDefine eventType, CallBack<T, X> callBack)
+    {
+        OnListenerAdding(eventType, callBack);
+        m_EventTable[eventType] = (CallBack<T, X>)m_EventTable[eventType] + callBack;
+    }
+    //three parameters
+    public static void AddListener<T, X, Y>(EventDefine eventType, CallBack<T, X, Y> callBack)
+    {
+        OnListenerAdding(eventType, callBack);
+        m_EventTable[eventType] = (CallBack<T, X, Y>)m_EventTable[eventType] + callBack;
+    }
+    //four parameters
+    public static void AddListener<T, X, Y, Z>(EventDefine eventType, CallBack<T, X, Y, Z> callBack)
+    {
+        OnListenerAdding(eventType, callBack);
+        m_EventTable[eventType] = (CallBack<T, X, Y, Z>)m_EventTable[eventType] + callBack;
+    }
+    //five parameters
+    public static void AddListener<T, X, Y, Z, W>(EventDefine eventType, CallBack<T, X, Y, Z, W> callBack)
+    {
+        OnListenerAdding(eventType, callBack);
+        m_EventTable[eventType] = (CallBack<T, X, Y, Z, W>)m_EventTable[eventType] + callBack;
+    }
+
+    //no parameters
+    public static void RemoveListener(EventDefine eventType, CallBack callBack)
+    {
+        OnListenerRemoving(eventType, callBack);
+        m_EventTable[eventType] = (CallBack)m_EventTable[eventType] - callBack;
+        OnListenerRemoved(eventType);
+    }
+    //single parameters
+    public static void RemoveListener<T>(EventDefine eventType, CallBack<T> callBack)
+    {
+        OnListenerRemoving(eventType, callBack);
+        m_EventTable[eventType] = (CallBack<T>)m_EventTable[eventType] - callBack;
+        OnListenerRemoved(eventType);
+    }
+    //two parameters
+    public static void RemoveListener<T, X>(EventDefine eventType, CallBack<T, X> callBack)
+    {
+        OnListenerRemoving(eventType, callBack);
+        m_EventTable[eventType] = (CallBack<T, X>)m_EventTable[eventType] - callBack;
+        OnListenerRemoved(eventType);
+    }
+    //three parameters
+    public static void RemoveListener<T, X, Y>(EventDefine eventType, CallBack<T, X, Y> callBack)
+    {
+        OnListenerRemoving(eventType, callBack);
+        m_EventTable[eventType] = (CallBack<T, X, Y>)m_EventTable[eventType] - callBack;
+        OnListenerRemoved(eventType);
+    }
+    //four parameters
+    public static void RemoveListener<T, X, Y, Z>(EventDefine eventType, CallBack<T, X, Y, Z> callBack)
+    {
+        OnListenerRemoving(eventType, callBack);
+        m_EventTable[eventType] = (CallBack<T, X, Y, Z>)m_EventTable[eventType] - callBack;
+        OnListenerRemoved(eventType);
+    }
+    //five parameters
+    public static void RemoveListener<T, X, Y, Z, W>(EventDefine eventType, CallBack<T, X, Y, Z, W> callBack)
+    {
+        OnListenerRemoving(eventType, callBack);
+        m_EventTable[eventType] = (CallBack<T, X, Y, Z, W>)m_EventTable[eventType] - callBack;
+        OnListenerRemoved(eventType);
+    }
+
+
+    //no parameters
+    public static void Broadcast(EventDefine eventType)
+    {
+        Delegate d;
+        if (m_EventTable.TryGetValue(eventType, out d))
+        {
+            CallBack callBack = d as CallBack;
+            if (callBack != null)
+            {
+                callBack();
+            }
+            else
+            {
+                throw new Exception(string.Format("Broadcast error：Event{0} has different type", eventType));
+            }
+        }
+    }
+    //single parameters
+    public static void Broadcast<T>(EventDefine eventType, T arg)
+    {
+        Delegate d;
+        if (m_EventTable.TryGetValue(eventType, out d))
+        {
+            CallBack<T> callBack = d as CallBack<T>;
+            if (callBack != null)
+            {
+                callBack(arg);
+            }
+            else
+            {
+                throw new Exception(string.Format("Broadcast error：Event{0} has different type", eventType));
+            }
+        }
+    }
+    //two parameters
+    public static void Broadcast<T, X>(EventDefine eventType, T arg1, X arg2)
+    {
+        Delegate d;
+        if (m_EventTable.TryGetValue(eventType, out d))
+        {
+            CallBack<T, X> callBack = d as CallBack<T, X>;
+            if (callBack != null)
+            {
+                callBack(arg1, arg2);
+            }
+            else
+            {
+                throw new Exception(string.Format("Broadcast error：Event{0} has different type", eventType));
+            }
+        }
+    }
+    //three parameters
+    public static void Broadcast<T, X, Y>(EventDefine eventType, T arg1, X arg2, Y arg3)
+    {
+        Delegate d;
+        if (m_EventTable.TryGetValue(eventType, out d))
+        {
+            CallBack<T, X, Y> callBack = d as CallBack<T, X, Y>;
+            if (callBack != null)
+            {
+                callBack(arg1, arg2, arg3);
+            }
+            else
+            {
+                throw new Exception(string.Format("Broadcast error：Event{0} has different type", eventType));
+            }
+        }
+    }
+    //four parameters
+    public static void Broadcast<T, X, Y, Z>(EventDefine eventType, T arg1, X arg2, Y arg3, Z arg4)
+    {
+        Delegate d;
+        if (m_EventTable.TryGetValue(eventType, out d))
+        {
+            CallBack<T, X, Y, Z> callBack = d as CallBack<T, X, Y, Z>;
+            if (callBack != null)
+            {
+                callBack(arg1, arg2, arg3, arg4);
+            }
+            else
+            {
+                throw new Exception(string.Format("Broadcast error：Event{0} has different type", eventType));
+            }
+        }
+    }
+    //five parameters
+    public static void Broadcast<T, X, Y, Z, W>(EventDefine eventType, T arg1, X arg2, Y arg3, Z arg4, W arg5)
+    {
+        Delegate d;
+        if (m_EventTable.TryGetValue(eventType, out d))
+        {
+            CallBack<T, X, Y, Z, W> callBack = d as CallBack<T, X, Y, Z, W>;
+            if (callBack != null)
+            {
+                callBack(arg1, arg2, arg3, arg4, arg5);
+            }
+            else
+            {
+                throw new Exception(string.Format("Broadcast error：Event{0} has different type", eventType));
+            }
+        }
+    }
+}
